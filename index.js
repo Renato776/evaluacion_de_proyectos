f14613c75e8ec63d69c9c815322b725da7629c09
Renato Flores <44614747+Renato776@users.noreply.github.com>
2020-04-13 11:58:29 -0700

    Started basic program flow

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+const my_args = process.argv.slice(2);
+if(my_args.length==0){
+    console.log('You must specify the path to your project. Usage: node index.js PATH');
+    process.exit(1);
+}
+const project_path = my_args[0];
+const fs = require('fs');
+const project = JSON.parse(fs.readFileSync(project_path));
+console.log(project);
+
